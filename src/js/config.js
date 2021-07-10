4e56f1f3f681f8b69b5a51cfda0896c411c03353
utkarsh-112 <utkarshkharkwal@gmail.com>
2021-07-10 14:32:31 +0530

    'updated'

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e67711ea-38ad-44d0-b889-75e3f0b3d054';
+export const MODAL_CLOSE_SEC = 2.5;
